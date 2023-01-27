57fac954ab836d4ebb5a32ea1603d544ec9d5991
Akram <khalilovakram07@gmail.com>
2023-01-27 22:02:43 +0600

    add tailwind.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
+import React, {createElement as e} from 'react';
 
 function App() {
 
-  const str: string = "Hello"
+  return e("div", {className: "container"}, [
+    e('button', {
+      className: 'py-2 px-4 border',
+      key: 2,
+      onClick: () => console.log("click")
+    }, "Click me!")
+  ])
 
-  console.log(str);
-  
-
-  return (
-    <h1>Hello World</h1>
-  )
 }
+
 
 export default App;
